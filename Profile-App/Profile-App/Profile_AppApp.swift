7f10a0657ce057b9c47ed4b45501fd4b0ce552ae
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-05-27 23:03:13 +0530

    Initial commit
    Initial commit for profile app.

---
+++ Profile-App/Profile-App/Profile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_AppApp.swift
+//  Profile-App
+//
+//  Created by Partha on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Profile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Profile-App/Profile-AppTests/Profile_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Profile_AppTests.swift
+//  Profile-AppTests
+//
+//  Created by Partha on 27/05/25.
+//
+
+import Testing
+
+struct Profile_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
