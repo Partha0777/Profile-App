5dea6168ac7c6b2155b5ead6fba72e2107beb2d2
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-05-28 21:28:11 +0530

    Profile
    Added profile top content.

--- Profile-App/Profile-App/ContentView.swift
+++ Profile-App/Profile-App/ContentView.swift
@@ -4,6 +4,11 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             Image("profileBg").resizable().edgesIgnoringSafeArea(.all)
+            VStack{
+                VStack(alignment: .center,spacing:20){
+                    
+                }
+            }
         }
     }
 }
