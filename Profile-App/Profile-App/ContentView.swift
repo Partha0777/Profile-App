49fedfb3a4bf427a2c657ca55b03ccaf1de231fd
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-05-28 21:46:34 +0530

    Profile
    Added profile role.

--- Profile-App/Profile-App/ContentView.swift
+++ Profile-App/Profile-App/ContentView.swift
@@ -5,13 +5,22 @@ struct ContentView: View {
         ZStack{
             Image("profileBg").resizable().edgesIgnoringSafeArea(.all)
             VStack{
-                VStack(alignment: .center,spacing:20) {
+                VStack(alignment: .center, spacing: 12) {
                     Image("profileImage").resizable()
                         .frame(width: 180, height: 180)
                         .aspectRatio(contentMode: .fit)
                         .clipShape(Circle())
                         .shadow(color: .red, radius: 5, x: 4, y:4)
                     Text("Partha").font(.system(size: 28, weight: Font.Weight.bold)).foregroundColor(Color.white)
+                        .shadow(radius: 4, x: 4, y: 4)
+                
+                    Text("Mobile App Engineer").font(.system(size: 18, weight: Font.Weight.regular)).foregroundColor(Color.white)
+                        .shadow(radius: 4, x: 4, y: 4)
+                    
+                    Text("").font(.system(size: 28, weight: Font.Weight.bold)).foregroundColor(Color.white)
+                        .shadow(radius: 4, x: 4, y: 4)
+                    
+                    Text("").font(.system(size: 28, weight: Font.Weight.bold)).foregroundColor(Color.white)
                         .shadow(radius: 4, x: 4, y: 4)
                }
             }
