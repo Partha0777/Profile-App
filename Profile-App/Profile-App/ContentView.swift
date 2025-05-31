344976c499f331e052c221e2a4766fc86742c2e8
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-05-31 12:56:40 +0530

    Profile
    Added about content

--- Profile-App/Profile-App/ContentView.swift
+++ Profile-App/Profile-App/ContentView.swift
@@ -8,6 +8,44 @@ struct ContentView: View {
             Image("profileBg").resizable().edgesIgnoringSafeArea(.all)
             VStack{
                 profile
+                
+                Spacer()
+                
+                VStack(alignment: .center, spacing: 20){
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 300, height: 50)
+                        .foregroundColor(.white)
+                        .shadow(color:.pink,radius: 5, x: 5, y:5)
+                        .overlay(
+                            Text("Follow")
+                                .foregroundColor(.pink)
+                                .font(.system(size: 22, weight: .bold))
+                        )
+                    
+                    HStack(spacing:80){
+                        
+                        VStack(spacing: 4){
+                            Text("2800").font(.system(size: 24, weight: .bold))
+                            Text("Followers").opacity(0.7)
+                        }
+                        
+                        VStack(spacing: 4){
+                            Text("535").font(.system(size: 24, weight: .bold))
+                            Text("Follows").opacity(0.7)
+                        }
+                        
+                        VStack(spacing: 4){
+                            Text("723").font(.system(size: 24, weight: .bold))
+                            Text("Posts").opacity(0.7)
+                        }
+                    }
+                    
+                    VStack(spacing: 12){
+                        Text("About").font(.system(size: 20, weight: .bold))
+                        Text("iOS Developer skilled in Swift, SwiftUI, and UIKit.Experienced in building clean, scalable apps with MVVM.Focus on performance, API integration, and smooth UX.").lineSpacing(4).opacity(0.7)
+                    }.padding(12)
+                    
+                }
             }
         }
         
@@ -16,6 +54,7 @@ struct ContentView: View {
     var profile: some View{
         
         VStack(alignment: .center, spacing: 12) {
+            Spacer()
             Image("profileImage").resizable()
                 .frame(width: 180, height: 180)
                 .aspectRatio(contentMode: .fit)
@@ -27,45 +66,12 @@ struct ContentView: View {
             Text("Mobile App Engineer").font(.system(size: 18, weight: Font.Weight.regular)).foregroundColor(Color.white)
                 .shadow(radius: 4, x: 4, y: 4)
             
-            Text("").font(.system(size: 28, weight: Font.Weight.bold)).foregroundColor(Color.white)
-                .shadow(radius: 4, x: 4, y: 4)
-            
-            Text("").font(.system(size: 28, weight: Font.Weight.bold)).foregroundColor(Color.white)
-                .shadow(radius: 4, x: 4, y: 4)
-            
+        
+    
            
             contact
             
-           
             
-            VStack(alignment: .center, spacing: 60){
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 300, height: 50)
-                    .foregroundColor(.white)
-                    .shadow(color:.pink,radius: 5, x: 5, y:5)
-                    .overlay(
-                        Text("Follow").font(.system(size: 22, weight: .bold))
-                    )
-                
-                HStack(spacing:60){
-                    
-                    VStack(spacing: 8){
-                        Text("2800").font(.system(size: 24, weight: .bold))
-                        Text("Followers")
-                    }
-                    
-                    VStack(spacing: 8){
-                        Text("535").font(.system(size: 24, weight: .bold))
-                        Text("Follows")
-                    }
-                    
-                    VStack(spacing: 8){
-                        Text("723").font(.system(size: 24, weight: .bold))
-                        Text("Posts")
-                    }
-                }
-                
-            }
        }
         
     }
@@ -93,6 +99,7 @@ struct ContentView: View {
         }.foregroundColor(.white)
             .shadow(color: .red, radius: 5,x: 5, y: 5)
             .frame(width: 250)
+            .padding()
     }
 }
 
